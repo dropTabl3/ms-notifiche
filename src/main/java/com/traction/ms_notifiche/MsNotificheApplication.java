d839213bb6a2202d20af1f7e2fe738f20f03a54c
Massimo Barbero <mxmo.barbero@gmail.com>
2025-02-23 17:04:46 +0100

    init project

---
+++ src/main/java/com/traction/ms_notifiche/MsNotificheApplication.java
@@ -0,0 +1,13 @@
+package com.traction.ms_notifiche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotificheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotificheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traction/ms_notifiche/MsNotificheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traction.ms_notifiche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
